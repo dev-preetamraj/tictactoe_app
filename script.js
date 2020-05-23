87b4bf17bed92c6c01442c6d7c26ce32f45090d3
Signior-X <b19188@students.iitmandi.ac.in>
2020-05-23 19:17:45 +0530

    Improved auto clean for game end

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
-	window.chance = 1;
-	
+	window.chance = 1; // For alternative chances of X and O Here, 1-> X, 0-> O 
+	window.totalChancesPlayed = 0; // For removing clearing the board automatically if the moves reach 9
+
 	var clearBoard = $(".btn");
 	var tdSelector = $("td");
 	var music = new Audio();
@@ -11,7 +12,7 @@ $(document).ready(function(){
 			tdSelector[i].textContent = '';
 		}
 		window.chance = 1;
-		result.innerHTML = '';
+		window.totalChancesPlayed = 0;
 	};
 	clearBoard.on("click", clearBord)
 	const array=["012","345","678","036","147","258","048","246"];
@@ -25,12 +26,16 @@ $(document).ready(function(){
 				&& (tdSelector[array[i][1]].textContent===tdSelector[array[i][2]].textContent) 
 				)
 			{
-				
 				result.innerHTML=tdSelector[array[i][0]].textContent+" WINS";
-				setTimeout(clearBord,500);
+				setTimeout(clearBord,500); // delay in between
+				// (I think that a proper way to ask if a new match should be started or not should be asked!)
 				result.innerHTML=tdSelector[array[i][0]].textContent+" WON The Last Match!";
 				
-			}	
+			} else if(window.totalChancesPlayed == 9){
+				
+				result.innerHTML="NO One WON The Last Match!";
+				setTimeout(clearBord,500);
+			}
 		}
 		
 	}
@@ -41,14 +46,17 @@ $(document).ready(function(){
 				this.value = 1;
 				this.style.color = '#007bff';
 				window.chance = 0;
+				window.totalChancesPlayed +=1;
+				music.play();
 			} else{
 				this.textContent = 'O';
 				this.value = 0;
 				this.style.color = '#94ffaa';
 				window.chance = 1;
+				window.totalChancesPlayed +=1;
+				music.play();
 			}
 		}
-		music.play();
 		checker();
 	}
 	for (var i = 0; i < tdSelector.length; i++) {
