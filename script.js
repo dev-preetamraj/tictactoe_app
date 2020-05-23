720269d15c98ce980fee961af0a2032f18de6027
Signior-X <b19188@students.iitmandi.ac.in>
2020-05-23 19:08:31 +0530

    Now alternative turns

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	window.chance = 1;
+	
 	var clearBoard = $(".btn");
 	var tdSelector = $("td");
 	var music = new Audio();
@@ -8,6 +10,8 @@ $(document).ready(function(){
 		for (var i = 0 ; i <=tdSelector.length-1 ; i++) {
 			tdSelector[i].textContent = '';
 		}
+		window.chance = 1;
+		result.innerHTML = '';
 	};
 	clearBoard.on("click", clearBord)
 	const array=["012","345","678","036","147","258","048","246"];
@@ -24,22 +28,27 @@ $(document).ready(function(){
 				
 				result.innerHTML=tdSelector[array[i][0]].textContent+" WINS";
 				setTimeout(clearBord,500);
+				result.innerHTML=tdSelector[array[i][0]].textContent+" WON The Last Match!";
+				
 			}	
 		}
 		
 	}
 	function addText(){
 		if(this.textContent===''){
-			this.textContent = 'X';
-			this.style.color = '#007bff';
-			music.play();
-		}else if(this.textContent==='X'){
-			this.textContent = 'O';
-			this.style.color = '#5eeb98'
-			music.play();
-		}else{
-			this.textContent = '';
+			if(window.chance == 1){
+				this.textContent = 'X';
+				this.value = 1;
+				this.style.color = '#007bff';
+				window.chance = 0;
+			} else{
+				this.textContent = 'O';
+				this.value = 0;
+				this.style.color = '#94ffaa';
+				window.chance = 1;
+			}
 		}
+		music.play();
 		checker();
 	}
 	for (var i = 0; i < tdSelector.length; i++) {
